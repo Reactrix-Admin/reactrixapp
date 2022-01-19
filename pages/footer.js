d684c663dfb9dcad19aa57bf44ac0de3cf22d219
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2022-01-18 18:27:37 -0800

    footer component created

---
+++ pages/footer.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css';
+
+function FooterComponent() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href='https://github.com/oslabs-beta/Reactrix'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Start Prototyping
+      </a>
+    </footer>
+  );
+}
+
+export default FooterComponent;
