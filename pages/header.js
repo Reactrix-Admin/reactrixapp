b0f954e742efbac92e3d629ad6d5fab62f15c197
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2022-01-18 17:48:56 -0800

    social media svgs added, css changes

---
+++ pages/header.js
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+import styles from '../styles/Home.module.css';
+
+function HeaderComponent() {
+  return (
+    <header className={styles.header}>
+      <a href='/' target='_blank' rel='noopener noreferrer'>
+        Reactrix{' '}
+      </a>
+      <div className={styles.social}>
+        <span className={styles.icon}>
+          <a href='/' target='_blank' rel='noopener noreferrer'>
+            <Image
+              src='/linkedin.svg'
+              alt='LinkedIn Logo'
+              width={24}
+              height={24}
+            />
+          </a>
+        </span>
+        <span className={styles.icon}>
+          <a
+            href='https://github.com/oslabs-beta/Reactrix'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Image src='/github.svg' alt='GitHub Logo' width={24} height={24} />
+          </a>
+        </span>
+      </div>
+    </header>
+  );
+}
+export default HeaderComponent;
